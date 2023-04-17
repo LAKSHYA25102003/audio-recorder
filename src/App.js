0527a873396644766795ab6f10fdeb61faacbbb8
LAKSHYA <llakshya63@gmail.com>
2023-04-17 13:47:28 +0530

    audio recorder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+
+  let audioRecorder = {
+    audioBlobs: [],
+    mediaRecorder: null,
+    streamBeingCaptured: null,
+
+    start: () => {
+      if (!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
+        return Promise.reject(new Error('mediaDevices API or getUserMedia method is not supported in this browser.'));
+      }
+      else {
+        return navigator.mediaDevices.getUserMedia({ audio: true })
+          .then(stream => {
+            audioRecorder.streamBeingCaptured = stream;
+
+
+            audioRecorder.mediaRecorder = new MediaRecorder(stream);
+
+
+            audioRecorder.audioBlobs = [];
+
+
+            audioRecorder.mediaRecorder.addEventListener("dataavailable", event => {
+
+              audioRecorder.audioBlobs.push(event.data);
+            });
+
+
+            audioRecorder.mediaRecorder.start();
+          });
+      }
+    },
+    
+    
+  }
+
+  const startAudioRecording = () => {
+    audioRecorder.start()
+      .then(() => {
+        console.log("Recording Audio...")
+      })
+      .catch(error => {
+        if (error.message.includes("mediaDevices API or getUserMedia method is not supported in this browser.")) {
+          console.log("To record audio, use browsers like Chrome and Firefox.");
+          
+          switch (error.name) {
+            case 'AbortError': //error from navigator.mediaDevices.getUserMedia
+              console.log("An AbortError has occured.");
+              break;
+            case 'NotAllowedError': //error from navigator.mediaDevices.getUserMedia
+              console.log("A NotAllowedError has occured. User might have denied permission.");
+              break;
+            case 'NotFoundError': //error from navigator.mediaDevices.getUserMedia
+              console.log("A NotFoundError has occured.");
+              break;
+            case 'NotReadableError': //error from navigator.mediaDevices.getUserMedia
+              console.log("A NotReadableError has occured.");
+              break;
+            case 'SecurityError': //error from navigator.mediaDevices.getUserMedia or from the MediaRecorder.start
+              console.log("A SecurityError has occured.");
+              break;
+            case 'TypeError': //error from navigator.mediaDevices.getUserMedia
+              console.log("A TypeError has occured.");
+              break;
+            case 'InvalidStateError': //error from the MediaRecorder.start
+              console.log("An InvalidStateError has occured.");
+              break;
+            case 'UnknownError': //error from the MediaRecorder.start
+              console.log("An UnknownError has occured.");
+              break;
+            default:
+              console.log("An error occured with the error name " + error.name);
+          };
+        };
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App text-center text-[24px] font-bold p-[10px]">
+      lakshya
     </div>
   );
 }
